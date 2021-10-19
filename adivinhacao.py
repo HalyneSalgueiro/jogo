3d24d01ce05c02e9ef9c233806cbbf194c1b686a
HalyneSalgueiro <halynesalgueiroalves@hotmail.com>
2021-10-19 15:03:25 -0300

    idade

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,9 +4,17 @@ print('*********************************')
 
 numero_secreto = 42
 
-chute = input('Digite o seu numero: ')
+chute_str = input('Digite o seu numero: ')
 
-print('Você digitou ', chute)
+print('Você digitou ', chute_str)
+
+chute = int(chute_str)
+
+if(numero_secreto == chute):
+    print('Você acertou')
+else:
+    print('Você errou!')
+
 
 
 
---
+++ idade_Fernanda.py
@@ -0,0 +1,7 @@
+minha_idade = 26
+idade_namorado = 25
+
+if(minha_idade == idade_namorado):
+    print('temos idades iguais')
+else:
+    print('temos idades diferentes')
