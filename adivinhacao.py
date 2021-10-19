2c00b3f880a5cef165170cc6d4c0eba90bc0d648
HalyneSalgueiro <halynesalgueiroalves@hotmail.com>
2021-10-19 13:29:21 -0300

    Initial commit

---
+++ adivinhacao.py
@@ -0,0 +1,14 @@
+print('*********************************')
+print('Bem vindo no jogo de Adivinhação!')
+print('*********************************')
+
+numero_secreto = 42
+
+chute = input('Digite o seu numero: ')
+
+print('Você digitou ', chute)
+
+
+
+
+
