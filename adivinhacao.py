569feef258037f6fd9f81d0a2a067041cd7a43d0
HalyneSalgueiro <halynesalgueiroalves@hotmail.com>
2021-10-19 17:50:46 -0300

    melhorando meu código

--- adivinhacao.py
+++ adivinhacao.py
@@ -10,10 +10,21 @@ print('Você digitou ', chute_str)
 
 chute = int(chute_str)
 
-if(numero_secreto == chute):
+acertou = chute == numero_secreto
+chute_foi_maior = chute > numero_secreto
+chute_foi_menor = chute < numero_secreto
+
+if(acertou):
     print('Você acertou')
 else:
-    print('Você errou!')
+    if(chute_foi_maior):
+       print('Você errou! O seu chute foi maior do que o número secreto.')
+    elif(chute_foi_menor):
+       print('Você errou! O seu chute foi menor do que o número secreto.')
+
+print('Fim do jogo')
+
+
 
 
 
