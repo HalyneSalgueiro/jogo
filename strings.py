4d6a7e904b6a4671621b266951ac8d6f13738d68
HalyneSalgueiro <halynesalgueiroalves@hotmail.com>
2021-10-20 13:05:41 -0300

    atividade usuário

--- strings.py
+++ strings.py
@@ -7,7 +7,7 @@ total_de_tentativas = 3
 rodada = 1
 
 while(rodada <= total_de_tentativas):
-    print('Tentativa', rodada, 'de', total_de_tentativas)
+    print('Tentativa {} de {}'.format(rodada, total_de_tentativas))
     chute_str = input('Digite o seu numero: ')
     print('Você digitou ', chute_str)
     chute = int(chute_str)
@@ -27,11 +27,5 @@ while(rodada <= total_de_tentativas):
     rodada = rodada + 1
 
     print('Fim do jogo')
-
-
-
-
-
-
 
 
