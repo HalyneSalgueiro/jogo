5f30c47187dd09ce920b12bf512a01c69cb861be
HalyneSalgueiro <halynesalgueiroalves@hotmail.com>
2021-11-03 15:35:21 -0300

    Adicionando níveis ao jogo laço com for

---
+++ adivinhacao_nivel.py
@@ -0,0 +1,46 @@
+import random
+
+print("*********************************")
+print("Bem vindo ao jogo de adivinhação!")
+print("*********************************")
+
+numero_secreto = random.randrange(1, 101)
+total_de_tentativas = 0
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil  (2) Médio  (3) Difícil")
+
+nivel = int(input("Escolha o nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str=input("Digite um número entre 0 e 100: ")
+    print("Você digitou ", chute_str)
+    chute=int(chute_str)
+
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
+    acertou=chute == numero_secreto
+    maior=chute > numero_secreto
+    menor=chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou!")
+        break
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto")
+
+
+print("Fim de jogo")
