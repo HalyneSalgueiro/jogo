b6ff41ea54accab09091d01d74d07d78d37cbb73
HalyneSalgueiro <halynesalgueiroalves@hotmail.com>
2021-11-19 23:14:41 -0300

    exercício 005 antecessor e sucessor

---
+++ ex005.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um número: '))
+a = n - 1
+s = n + 1
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n,a,s))
+
+# outra forma de escrever a variante (n, a, s)
+# n = int(input('Digite um número: '))
+# print(Anlisando o valor{}, seu antecessor é {} e o sucessor é {}' .format(n-1), (n+1)))
