f6a110cf793a724e1e17a518800eaf172a1c5afc
HalyneSalgueiro <halynesalgueiroalves@hotmail.com>
2021-11-25 20:16:21 -0300

    ex005

--- ex005.py
+++ ex005.py
@@ -3,6 +3,3 @@ a = n - 1
 s = n + 1
 print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n,a,s))
 
-# outra forma de escrever a variante (n, a, s)
-# n = int(input('Digite um número: '))
-# print(Anlisando o valor{}, seu antecessor é {} e o sucessor é {}' .format(n-1), (n+1)))
